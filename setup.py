8cc4ab9eb76d196f1e5234ad993966873287ee9a
pokiemaster <64101098+pokiemaster@users.noreply.github.com>
2022-09-25 12:04:35 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 with open("data/config.json", "a") as file:
-    print("Input your discord authorization token:")
+    print("Input your discord authorization token:OTc1NDU2NzE2MjgyMDM2MzE1.G8JeWp.hKDb0LrmiJw0vPimyM-OFzfJQfpVown_mGsDe0")
     auth_token = input()
     file.write("{\n")
     file.write(f'   "user_token" : "{auth_token}",\n')
-    print("Now, input the preferred Channel ID for spamming and catching:")
+    print("Now, input the preferred Channel ID for spamming and catching:1000896531018305587")
     channel_id = input()
     file.write(f'   "channel_id" : "{channel_id}"\n')
     file.write("}")
